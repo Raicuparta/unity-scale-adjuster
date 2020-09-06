00eb6a46cb55bc964c329504edf14e95eb93681f
Ricardo Lopes <raicuparta@gmail.com>
2020-09-06 11:07:14 +0200

    Initial commit

---
+++ UnityScaleAdjuster/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using Raicuparta.UnityScaleAdjuster;
+
+[assembly: MelonInfo(typeof(UnityScaleAdjuster), "UnityScaleAdjuster", "0.1.0", "Raicuparta")]
+[assembly: MelonGame(null, null)]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UnityScaleAdjuster")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnityScaleAdjuster")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d0ce196a-e8f0-48af-a72f-fb42cbbe6d81")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnityScaleAdjuster/UnityScaleAdjuster.cs
@@ -0,0 +1,26 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace Raicuparta.UnityScaleAdjuster
+{
+    public class UnityScaleAdjuster : MelonMod
+    {
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.F4))
+            {
+                MultiplyCameraScale(0.9f);
+            }
+            if (Input.GetKeyDown(KeyCode.F3))
+            {
+                MultiplyCameraScale(1.1f);
+            }
+        }
+
+        private void MultiplyCameraScale(float scale)
+        {
+            Camera.main.transform.localScale *= scale;
+            MelonLogger.Log($"Change camera scale to {Camera.main.transform.localScale}");
+        }
+    }
+}
